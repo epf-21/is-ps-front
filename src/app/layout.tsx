7e63408270db69f8b4cac1118d07f18ba6e72738
efrain <efrainperalta.f@gmail.com>
2025-04-23 09:47:25 -0400

    solucion del search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/provider/provider";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,7 +25,9 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         <Providers>
-          {children}
+          <Suspense fallback={<div>Cargando...</div>}>
+            {children}
+          </Suspense>
         </Providers>
       </body>
     </html>

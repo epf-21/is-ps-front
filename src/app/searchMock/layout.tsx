c1e4f01e07555c79023bd0be30685688a596e593
efrain <efrainperalta.f@gmail.com>
2025-04-23 09:42:51 -0400

    arreglos

---
+++ src/app/searchMock/layout.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from "react"
+
+export default function SearchLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<div>Cargando búsqueda...</div>}>
+      {children}
+    </Suspense>
+  )
+}

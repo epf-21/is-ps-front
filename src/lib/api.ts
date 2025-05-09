ca38d41a2766906e5bc5d683f84f1e3861417356
efrain <efrainperalta.f@gmail.com>
2025-05-09 14:00:21 -0400

    login

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 // lib/api.js
 import axios from 'axios';
+import { API_URL } from '@/utils/bakend';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/login/api', //URL de producción revisar porque no me acuerdo
+  baseURL: API_URL + '/login/api', //URL de producción revisar porque no me acuerdo
 });
 
 export default api;

a6be0909edfc307fe8ba282153705c6a5c1f3c93
efrain <efrainperalta.f@gmail.com>
2025-05-11 17:53:18 -0400

    arreglitos

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,10 +3,7 @@ import axios from 'axios';
 import { API_URL } from '@/utils/bakend';
 
 const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: API_URL + '/login/api',
 });
 
 export default api;

5d64ca8c4bb2e98f6ad00f8365c50c0a0d0d243e
efrain <efrainperalta.f@gmail.com>
2025-05-16 06:06:05 -0400

    detalles del radio

--- src/components/map/components/PuntoDinamico.tsx
+++ src/components/map/components/PuntoDinamico.tsx
@@ -1,32 +1,36 @@
 import { LatLng } from 'leaflet'
 import React, { useState } from 'react'
-import { Marker, useMapEvents, Circle} from 'react-leaflet'
+import { Marker, useMapEvents, Circle } from 'react-leaflet'
 
 interface HijoProps {
   radio: number,
-  punto: {lon:number,alt:number},
-  setpunto: (punto:{lon:number,alt:number}) => void;
+  punto: { lon: number, alt: number },
+  setpunto: (punto: { lon: number, alt: number }) => void;
 }
-const MapPunto= ({radio,punto,setpunto }:HijoProps) => {
+const MapPunto = ({ radio, punto, setpunto }: HijoProps) => {
   const [position, setPosition] = useState<LatLng | null>(null)
   const [ubicacionEnviada, setUbicacionEnviada] = useState(false);
-  function actualizarPunto(lon:number,alt:number) {
-    setpunto({lon,alt})
+  function actualizarPunto(lon: number, alt: number) {
+    setpunto({ lon, alt })
   }
   const map = useMapEvents({
     click(e) {
       const { lat, lng } = e.latlng;
       setPosition(e.latlng);
-      actualizarPunto(lng,lat);
+      actualizarPunto(lng, lat);
       map.locate()
-      map.flyTo(e.latlng, map.getZoom())
+      map.flyTo(e.latlng, map.getZoom(), {
+        duration: 0.7,
+      })
     },
     locationfound(e) {
       if (!ubicacionEnviada) {
         const { lat, lng } = e.latlng;
         setPosition(e.latlng);
         actualizarPunto(lng, lat);
-        map.flyTo(e.latlng, map.getZoom());
+        map.flyTo(e.latlng, map.getZoom(), {
+          duration: 0.7
+        });
         setUbicacionEnviada(true);
       }
     },
@@ -39,7 +43,11 @@ const MapPunto= ({radio,punto,setpunto }:HijoProps) => {
       {
         position && (
           <div>
-            <Circle center={position} pathOptions={fillBlueOptions} radius={radio * 1000} />
+            <Circle
+              center={position}
+              pathOptions={fillBlueOptions}
+              radius={radio * 1000}
+            />
             <Marker position={position} draggable={false}></Marker>
           </div>
         )
